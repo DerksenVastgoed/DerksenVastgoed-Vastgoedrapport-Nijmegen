73c12ace2d39dd3f1ddad47fe3fee04a75bdcc3c
DerksenVastgoed <mark@derksen-vastgoed.nl>
2026-09-21 18:20:09 +0200

    brief_verhalend.py

--- brief_verhalend.py
+++ brief_verhalend.py
@@ -85,6 +85,8 @@ WAT JE NIET DOET:
 
 LET OP BIJ PERCENTAGES. Een percentage achter een pand is de afwijking van de MEDIAANPRIJS PER VIERKANTE METER in die buurt, niet een prijswijziging. "Nieuwe Markt 90 €575.000 (-27%)" betekent dus: dit pand is per vierkante meter 27% goedkoper dan vergelijkbare panden in die buurt. Het betekent NIET dat de vraagprijs verlaagd is. Schrijf dus nooit "onder de oorspronkelijke vraagprijs" of "inmiddels verhoogd". Een echte prijswijziging staat er altijd expliciet bij als "prijs verlaagd met" of "prijs gewijzigd".
 
+GEEN TOEZEGGINGEN NAMENS MARK. De brief is van Mark, maar jij beslist niet wat hij gaat doen. Schrijf dus niet "ik ga dat voortaan standaard doen" of "dat voeg ik toe aan onze lijst". Je mag zeggen wat je opvalt en wat het overwegen waard is; wat hij ermee doet is aan hem.
+
 DE RENTE IS DE MARKTRENTE, NIET DIE VAN ONS. De rentecijfers in de gegevens zijn de tarieven die een bank vandaag rekent voor een nieuwe verhuurhypotheek, per financieringsgraad. Het is NIET de rente op de eigen portefeuille: die is vast gefinancierd en beweegt niet mee met de markt. Schrijf dus nooit "onze rente", "bij ons staat hij", "onze hypotheek" of "onze financiering" als je deze cijfers bedoelt. Zeg "de marktrente voor een verhuurhypotheek" of "wat een bank nu rekent".
 
 Trek er ook geen conclusie uit voor het bestaande bezit. Een stijgende marktrente maakt een NIEUWE aankoop duurder en drukt de prijs die je kunt bieden; op de panden die er al zijn heeft hij geen invloed zolang de rente vaststaat. Noem nooit de voorwaarden of de herkomst van de eigen financiering: die horen niet in de brief.
